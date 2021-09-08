8c30942fa9dc0725f8d7f3528bab00b552657f74
Šarūnas Navickas <zaibacu@gmail.com>
2021-09-08 20:00:06 +0300

    hackish way to lazy load JSON

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use structopt::StructOpt;
 use std::fs::OpenOptions;
 
 use std::fs::{File, remove_file};
-use std::io::prelude::*;
 use std::io::Write;
+use std::io::ErrorKind;
 use std::path::Path;
 use read_iter::ReadIter; // also add dependency to Cargo.toml
 use nop_json::{Reader, Value};
+
+use std::io::BufRead;
+use std::io::BufReader;
+use std::str;
 
 
 #[derive(StructOpt)]
@@ -29,19 +33,48 @@ fn main() {
     }
 
     let mut out_file = OpenOptions::new()
-      .write(true)
-      .create_new(true)
-      .append(true)
-      .open(&args.output)
-      .unwrap();
+        .write(true)
+        .create_new(true)
+        .append(true)
+        .open(&args.output)
+        .unwrap();
+
 
     let mut file = ReadIter::new(File::open(args.path).unwrap());
-    let mut reader = Reader::new(&mut file);
+    let mut data_iter = file.skip(1);
+    let mut reader = Reader::new(&mut data_iter);
 
+    loop {
+        match reader.read::<Value>() {
+            Ok(value) => {
+                write!(out_file, "{}\n", value);
+            },
+            Err(ref e) if e.kind() == ErrorKind::UnexpectedEof => break,
+            Err(ref e) if e.kind() == ErrorKind::InvalidInput => break,
+            Err(ref e) if e.kind() == ErrorKind::Other => {
+                if e.to_string() == "$: Invalid JSON: unexpected end of input"{
+                    break
+                }
+                else if e.to_string() == "$: Invalid JSON input: unexpected ','"{
+                    continue
+                }
+                else if e.to_string() == "$: Invalid JSON input: unexpected ']'"{
+                    continue
+                }
+                else {
+                    println!("{}", e.to_string());
+                    continue
+                }
+                
+            },
+            Err(e) => eprintln!("Error: {}, kind: {:?}", e, e.kind()),
+        }
+    }
+    /*
     let root: Vec<Value> = reader.read().unwrap();
     for value in root {
         write!(out_file, "{}\n", value);
-    }
+    }*/
     /*for value in stream {
         println!("{}", value.unwrap());
     }*/
